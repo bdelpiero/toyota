c702f93a525071ecc398e1f20f91c50eb8b0e56c
Bruno Del Piero <bdelpiero93@gmail.com>
2021-02-24 23:20:53 -0300

    reloj empezado

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+// https://codepen.io/AllThingsSmitty/pen/JJavZN
+
+(function () {
+  const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
+
+  let birthday = "Mar 11, 2021 00:00:00",
+    countDown = new Date(birthday).getTime(),
+    x = setInterval(function () {
+      let now = new Date().getTime(),
+        distance = countDown - now;
+
+      (document.getElementById("days").innerText = Math.floor(distance / day)),
+        (document.getElementById("hours").innerText = Math.floor(
+          (distance % day) / hour
+        )),
+        (document.getElementById("minutes").innerText = Math.floor(
+          (distance % hour) / minute
+        )),
+        (document.getElementById("seconds").innerText = Math.floor(
+          (distance % minute) / second
+        ));
+
+      //do something later when date is reached
+      // if (distance < 0) {
+
+      //   clearInterval(x);
+      // }
+    }, 0);
+})();
+
 function openForm() {
   const form = document.querySelector(".main-form");
   const counter = document.querySelector(".main-countdown");
@@ -13,15 +46,3 @@ function closeForm() {
   form.style.display = "none";
   counter.style.display = "block";
 }
-
-// function toggleVisibility(className) {
-//     console.log(className)
-//     const element = document.querySelector(className);
-//     console.log(element)
-
-//     if (element.style.display === "none") {
-//         element.style.display = "block";
-//     } else {
-//         element.style.display = "none";
-//     }
-// }
