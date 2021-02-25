5c4fc2218343fd3fc67ddae57855aaeab6af640d
Bruno Del Piero <bdelpiero93@gmail.com>
2021-02-24 22:50:25 -0300

    mobile primera version

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
 function openForm() {
-    const form = document.querySelector(".main-form");
-    const counter = document.querySelector(".main-countdown");
- 
-    form.style.display = "flex";
-    counter.style.display = "none";
+  const form = document.querySelector(".main-form");
+  const counter = document.querySelector(".main-countdown");
 
+  form.style.display = "flex";
+  counter.style.display = "none";
 }
 
 function closeForm() {
-    const form = document.querySelector(".main-form");
-    const counter = document.querySelector(".main-countdown");
+  const form = document.querySelector(".main-form");
+  const counter = document.querySelector(".main-countdown");
 
-    form.style.display = "none";
-    counter.style.display = "block";
-
+  form.style.display = "none";
+  counter.style.display = "block";
 }
-
-
 
 // function toggleVisibility(className) {
 //     console.log(className)
 //     const element = document.querySelector(className);
 //     console.log(element)
-    
+
 //     if (element.style.display === "none") {
 //         element.style.display = "block";
 //     } else {
